28a527ab92a7008775c6a679d7f548006ef81602
Christoph Gruener <chrstphgrnr@Christophs-MacBook-Pro.local>
2020-04-01 16:07:49 -0600

    add more tests to Browser test spec

--- cypress/integration/examples/browserTest.spec.js
+++ cypress/integration/examples/browserTest.spec.js
@@ -2,4 +2,16 @@ describe('Browswer Actions', () => {
 	it('should load correct URL', () => {
 		cy.visit('http://example.com', { timeout: 10000 })
 	})
+
+	it('should check correct URL', () => {
+		cy.url().should('include', 'example.com')
+	})
+
+	it('should wait for 3 seconds', () => {
+		cy.wait(3000)
+	})
+
+	it('should check for correct element on the page', () => {
+		cy.get('h1').should('be.visible')
+	})
 })
